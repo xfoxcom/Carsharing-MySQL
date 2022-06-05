d3ce5da3cead7e352eb8081df39526120f4f4dfa
Alexander Boev <97745889+xfoxcom@users.noreply.github.com>
2022-06-05 13:36:19 +0300

    Add files via upload

--- Customer.java
+++ Customer.java
@@ -2,13 +2,13 @@ package carsharing;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Scanner;
+import java.util.*;
 
 public class Customer {
     public static void createCustomer() {
         Scanner scanner = new Scanner(System.in);
         DBconnect.connect();
-        System.out.println("\nEnter customer name: ");
+        System.out.println("\nEnter the customer name: ");
         String name = scanner.nextLine();
         try {
             DBconnect.statement.executeUpdate("INSERT INTO CUSTOMER (NAME) VALUES ('" + name + "');");
@@ -28,7 +28,7 @@ DBconnect.disconnect();
                 i++;
             }
             if (i == 0) {
-                System.out.println("The customer list is empty!");
+                System.out.println("\nThe customer list is empty!\n");
             } else {
 
                 System.out.println("\nCustomer list: ");
@@ -78,34 +78,38 @@ DBconnect.disconnect();
                             System.out.println(choose.getInt("ID") + ". " + choose.getString("NAME"));
                         }
                         System.out.println("0. Back");
-                        int ch = scanner.nextInt();
+                        int ch = scanner.nextInt(); // id of company
                         if (ch == 0) {
                             System.out.println();
                         } else {
                             System.out.println("\nChoose a car: ");
-                            ResultSet name = DBconnect.statement.executeQuery("SELECT NAME FROM COMPANY WHERE ID = " + ch);
-                            String compN = "";
-                            if (name.next()) {
-                                compN = name.getString("NAME");
-                            }
-                            ResultSet car = DBconnect.statement.executeQuery("SELECT ID, NAME FROM CAR_" + compN);
-                            while (car.next()) {
-                                System.out.println(car.getInt("ID") + ". " + car.getString("NAME"));
-                            }
+                            String customersChoose = "";
+                            int f = 1;
+                            Map<Integer, String> cars = new LinkedHashMap<>();
+                                ResultSet car1 = DBconnect.statement.executeQuery("SELECT NAME FROM CAR WHERE COMPANY_ID = " + ch + " AND RENTED IS NOT TRUE");
+                                while (car1.next()) {
+                                    cars.put(f++, car1.getString("NAME"));
+                                }
+                                for (Map.Entry<Integer, String> entry : cars.entrySet()) {
+                                    System.out.println(entry.getKey() + ". " + entry.getValue());
+                                }
+                          //  }
                             System.out.println("0. Back");
                             int enter = scanner.nextInt();
-                            if (enter == 0) { // car's id
+                            if (enter == 0) { // car's key
                                 System.out.println();
                             } else {
-                                ResultSet resultSet = DBconnect.statement.executeQuery("SELECT NAME FROM CAR_" + compN + " WHERE ID = " + enter);
-                                if (resultSet.next()) {
-                                    String a = resultSet.getString("NAME");
-                                    System.out.println("\nYou rented '" + a + "'");
-                                    ResultSet resultSet1 = DBconnect.statement.executeQuery("SELECT ID FROM CAR WHERE NAME = '" + a + "'");
-                                    if (resultSet1.next()) {
-                                        DBconnect.statement.executeUpdate("UPDATE CUSTOMER SET RENTED_CAR_ID = " + resultSet1.getInt("ID"));
+                                for (Map.Entry<Integer, String> entry : cars.entrySet()) {
+                                    if (entry.getKey() == enter) {
+                                        customersChoose = entry.getValue();
                                     }
                                 }
+                                    System.out.println("\nYou rented '" + customersChoose + "'");
+                                    ResultSet resultSet1 = DBconnect.statement.executeQuery("SELECT ID FROM CAR WHERE NAME = '" + customersChoose + "'");
+                                    if (resultSet1.next()) {
+                                        DBconnect.statement.executeUpdate("UPDATE CUSTOMER SET RENTED_CAR_ID = " + resultSet1.getInt("ID") + " WHERE ID = " + id);
+                                        DBconnect.statement.executeUpdate("UPDATE CAR SET RENTED = TRUE WHERE NAME = '" + customersChoose + "'");
+                                    }
                             }
                         }
                         }
@@ -113,26 +117,51 @@ DBconnect.disconnect();
                     } catch(SQLException e){
                         e.printStackTrace();
                     }
-
             }
             if (k == 2) {
+                boolean OK = true;
                 try {
                     ResultSet isRent = DBconnect.statement.executeQuery("SELECT RENTED_CAR_ID FROM CUSTOMER WHERE ID = " + id);
                     if (isRent.next()) {
                         if (isRent.getInt("RENTED_CAR_ID") == 0) {
+                            OK = false;
                             System.out.println("\nYou didn't rent a car!");
                         }
+                    }
+                    if (OK){
+                        DBconnect.statement.executeUpdate("UPDATE CUSTOMER SET RENTED_CAR_ID = NULL");
+                        System.out.println("\nYou've returned a rented car!");
                     }
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
             }
             if (k == 3) {
+                boolean OK = true;
+                int RCI = 0;
                 try {
                     ResultSet isRent = DBconnect.statement.executeQuery("SELECT RENTED_CAR_ID FROM CUSTOMER WHERE ID = " + id);
                     if (isRent.next()) {
                         if (isRent.getInt("RENTED_CAR_ID") == 0) {
+                            OK = false;
                             System.out.println("\nYou didn't rent a car!");
+                        }
+                    }
+                    if (OK) {
+                        ResultSet rented = DBconnect.statement.executeQuery("SELECT RENTED_CAR_ID FROM CUSTOMER WHERE ID = " + id);
+                        if (rented.next()) {
+                            RCI = rented.getInt("RENTED_CAR_ID");
+                        }
+                            rented = DBconnect.statement.executeQuery("SELECT NAME FROM CAR WHERE ID = " + RCI);
+                            if (rented.next()) {
+                                System.out.println("\nYour rented car: \n" + rented.getString("NAME"));
+                            }
+                            rented = DBconnect.statement.executeQuery("SELECT COMPANY_ID FROM CAR WHERE ID = " + RCI);
+                        if (rented.next()) {
+                           ResultSet rented1 = DBconnect.statement.executeQuery("SELECT NAME FROM COMPANY WHERE ID = " + rented.getInt("COMPANY_ID"));
+                           if (rented1.next()) {
+                               System.out.println("Company: \n" + rented1.getString("NAME"));
+                           }
                         }
                     }
                 } catch (SQLException e) {
